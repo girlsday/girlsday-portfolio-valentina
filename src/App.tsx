a39bc4dd8092b3011d71c95d9feca4e519149239
girlsday <girlsday@bitgrip.de>
2023-04-27 15:37:05 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Card, Container, MantineProvider, Text, Title } from "@mantine/core";
+import { Button, Card, Container, List, MantineProvider, Text, Title, Image, Center } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,26 +10,38 @@ function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Container>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <Title order={1}>Vite + React</Title>
-        <Card mt="xl">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <Text mt="sm">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </Text>
-        </Card>
-        <Text mt="md" c="dimmed">
-          Click on the Vite and React logos to learn more
+        <Title order={1}color="#1864AB" >
+Das ich 
+          </Title>
+          <Center>
+          <Image mb={50} src="/girlsday-portfolio-valentina/GirlsDay-13.jpg" maw={300}/>
+          </Center>
+        <Button component="a" href="https://www.instagram.com/valentina18.0909/" target="_blank" variant="light" color="indigo" size="xl" uppercase>
+          <Text color="#1864AB"> 
+            valle die qualle
+            </Text>
+        </Button>
+        <Text mt="md" c="dark" size={30}>
+          valentina18.0909
         </Text>
+        <Title order={1}>
+
+</Title>
+<Text mt="md"mb={30} c="dark"size={20}>
+i love and hate my life at the same time
+</Text>
+<Carousel  loop mb={25} c="dark" size={20}>
+          <Carousel.Slide>Hobbys</Carousel.Slide>
+          <Carousel.Slide>Hip Hop</Carousel.Slide>
+          <Carousel.Slide>Tennis</Carousel.Slide>
+          <Carousel.Slide>Klavier</Carousel.Slide>
+          <Carousel.Slide>Freunde treffen</Carousel.Slide>
+          <Carousel.Slide>Familie treffen</Carousel.Slide>
+          <Carousel.Slide>Feiern zu gehen</Carousel.Slide>
+        </Carousel>
+        <Text mt="md"mb={30} size={20} c="dark">
+ich bin geboren um meine jugend zu leben 
+</Text>
       </Container>
     </MantineProvider>
   );
